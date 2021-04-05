2b69cd8c457699cab980c35aa5ea8ba9742bc130
andrew <a.oleyn1kov@outlook.com>
2021-04-06 00:59:44 +0300

    numerical algorithm

---
+++ numericalalgorithm/src/main/java/com/polynomial/numerical/Main.java
@@ -0,0 +1,20 @@
+package com.polynomial.numerical;
+
+import com.polynomial.numerical.algorithm.calculator.Processor;
+import com.polynomial.numerical.algorithm.custom.CustomThreadPool;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+    }
+}
